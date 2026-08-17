e0b5bc1e280f433c80c75898562120f8362ec6bd
ErickSiedler <ericksiedler@gmail.com>
2026-08-17 20:12:54 -0300

    alter width from table

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         </div>
     </header>
 
-    <main class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+    <main class="mx-auto @yield('main-width', 'max-w-7xl') px-4 py-8 sm:px-6 lg:px-8">
         @if (session('success'))
             <div class="mb-6 rounded-lg border border-emerald-200 bg-emerald-50 px-4 py-3 text-sm text-emerald-800">
                 {{ session('success') }}
